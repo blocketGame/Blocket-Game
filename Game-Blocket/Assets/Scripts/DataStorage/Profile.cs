4656abbbe6124b5efdbdbcf333ea11c7a29ca9ba
Cesarich Philipp <59655496+Cse19455@users.noreply.github.com>
2022-03-07 19:27:27 +0100

    Feature/settingsprofilerecharged (#55)
    * Wizardry
    
    * overwritten
    
    Co-authored-by: Fabian Maurutschek <mau18927@spengergasse.at>

--- Game-Blocket/Assets/Scripts/DataStorage/Profile.cs
+++ Game-Blocket/Assets/Scripts/DataStorage/Profile.cs
@@ -24,33 +24,48 @@ public class SettingsProfile : Profile{
 	/// <summary>
 	/// Default Keys for the whole game
 	/// </summary>
-	public Dictionary<string, KeyCode> Keys { private get; set; } = new Dictionary<string, KeyCode>() {
-		///Default Keys:
-		//UI
-		{"InventoryKey",KeyCode.E},
-		{"ChatKey", KeyCode.T },
-
-		//Mechanics
-		{"MainInteractionKey", KeyCode.Mouse0 },
-		{"SideInteractionKey", KeyCode.Mouse1 },
-		{"JumpKey", KeyCode.Space },
-		{"RollKey", KeyCode.LeftControl },
-		{"CrawlKey", KeyCode.LeftShift } ,
-		{"CraftingInterface", KeyCode.F }
-	};
-
-	/// <summary>
-	/// Checks if special UI is open and resitricts imput
-	/// </summary>
-	/// <param name="str">Name of the Command</param>
-	/// <returns>keycode if exists</returns>
-	public KeyCode GetKeyCode(string str){
-		if(!GlobalVariables.UIInventory.ChatOpened)
-			return Keys[str];
-		return KeyCode.None;
+	public void LockKeysExceptSpecified(KeyCode k)
+    {
+		KeyValues.ForEach(v => 
+		v.MappedKey = (v.MappedKey == k ? k : KeyCode.None));
     }
+	public static List<KeyTupple> FillKeyValues()
+	{
+		return new List<KeyTupple> {
+			new KeyTupple("InventoryKey", KeyCode.E),
+			new KeyTupple("ChatKey", KeyCode.T),
+			new KeyTupple("MainInteractionKey", KeyCode.Mouse0),
+			new KeyTupple("SideInteractionKey", KeyCode.Mouse1),
+			new KeyTupple("JumpKey", KeyCode.Space),
+			new KeyTupple("RollKey", KeyCode.LeftControl),
+			new KeyTupple("CrawlKey", KeyCode.LeftShift),
+			new KeyTupple("CraftingInterface", KeyCode.F)
+		};
+	} 
+	public List<KeyTupple> KeyValues { private get; set; } = FillKeyValues();
+
+	public KeyCode InventoryKey => KeyValues[0].MappedKey;
+	public KeyCode ChatKey => KeyValues[1].MappedKey;
+	public KeyCode MainInteractionKey => KeyValues[2].MappedKey;
+	public KeyCode SideInteractionKey => KeyValues[3].MappedKey;
+	public KeyCode JumpKey => KeyValues[4].MappedKey;
+	public KeyCode RollKey => KeyValues[5].MappedKey;
+	public KeyCode CrawlKey => KeyValues[6].MappedKey;
+	public KeyCode CraftingInterface => KeyValues[7].MappedKey;
+
 
 	public SettingsProfile(string name, int? profileHash) : base(name, profileHash) { }
 
+}
+
+public struct KeyTupple
+{
+	public string KeyName { get; set; }
+	public KeyCode MappedKey { get; set; }
+    public KeyTupple(string keyName , KeyCode mappedKey)
+    {
+		this.KeyName = keyName ?? string.Empty;
+		this.MappedKey = mappedKey;
+    }
 }
 
