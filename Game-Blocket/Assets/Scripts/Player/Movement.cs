4b220f02f4fd29d616e769b313c674af17f4affa
Cesarich Philipp <59655496+Cse19455@users.noreply.github.com>
2022-03-07 14:24:42 +0100

    Restructured_Movement (#52)

--- Game-Blocket/Assets/Scripts/Player/Movement.cs
+++ Game-Blocket/Assets/Scripts/Player/Movement.cs
@@ -104,6 +104,7 @@ public class Movement : MonoBehaviour {
     {
 		VelocityUpdate();
 		MoveHorizontally();
+
 		if (PlayerVariables.Gamemode != Gamemode.CREATIVE)
 		{
 			FallAcceleration();
--- Game-Blocket/Assets/Scripts/Structure/Structure.cs
+++ Game-Blocket/Assets/Scripts/Structure/Structure.cs
@@ -2,7 +2,7 @@ using System;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-[SerializeField]
+[Serializable]
 public class Structure : MonoBehaviour
 {
     public byte id;
